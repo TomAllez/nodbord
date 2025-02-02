55bf9aabf4f6ea1ba56690dbfa4c347925cd5acf
TomAllez <allezthomas@gmail.com>
2025-02-02 19:50:05 +0100

    feat(node):implement basi nodes and cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,40 @@
-import { createRoot } from 'react-dom/client'
-import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const container = document.getElementById('root') as HTMLDivElement
-const root = createRoot(container)
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { WorkSpace } from './views'
+import Node, { InputOutput, NodeInterface } from 'components/node'
 
-root.render(<App />)
+const SampleAdditionNode: NodeInterface<number> = {
+  title: 'Addition',
+  inputs: [
+    { name: 'a', value: 1 },
+    { name: 'b', value: 2 }
+  ],
+  outputs: [{ name: 'result', value: 0 }],
+  logic: (inputs: InputOutput<number>[]): InputOutput<number>[] => {
+    // Perform addition logic here
+    const sum = inputs.reduce((acc, input) => acc + input.value, 0)
+    return [{ name: 'result', value: sum }]
+  }
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <WorkSpace>
+              <Node {...SampleAdditionNode} />
+            </WorkSpace>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
+
+postMessage({ payload: 'removeLoading' }, '*')
